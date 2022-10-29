e3916bd6f3236469efb0a5910554c86292c57e5c
Harsh Katkade <hkatkade7@gmail.com>
2022-10-29 17:22:56 +0530

    added zscore generation view

--- movieRec/z_score_pred/views.py
+++ movieRec/z_score_pred/views.py
@@ -1,3 +1,11 @@
+from zlib import Z_NO_COMPRESSION
 from django.shortcuts import render
+import scipy.stats as stats
 
 # Create your views here.
+class ZScoreGen:
+    def calculate_z_score(self,x):
+        if len(x):
+            zscores = stats.zscore(x)  
+            return zscores
+        return -1
