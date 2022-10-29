ba06c38df564dc4f06615772c3a51b50bcfae715
Harsh Katkade <hkatkade7@gmail.com>
2022-10-29 17:30:47 +0530

    added a model for predicted score

--- movieRec/z_score_pred/models.py
+++ movieRec/z_score_pred/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class MovieGen(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    rating = models.FloatField()
+    zscore = models.FloatField()
+    gen = models.IntegerField()
+    docs = models.ImageField()
+
+    @classmethod
+    def save():
+        super.save()
+        return "Saved signal generated"
